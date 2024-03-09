d9851a00b9a5a74975243b9638ad87d860e8878f
ProitV <proit@mail.ru>
2024-03-10 02:20:10 +0700

    Update handler2.py

--- handler2.py
+++ handler2.py
@@ -7,7 +7,7 @@ def sample1_on_create2(hashMap,_files=None,_data=None):
   return hashMap
 
 def sample1_on_input2(hashMap,_files=None,_data=None):
-  if hashMap.get("listener")=="btn_account":
+  if hashMap.get("listener")=="btn_acount":
       hashMap.put("result",str(int(hashMap.get("a"))-int(hashMap.get("b"))))
 
   return hashMap
